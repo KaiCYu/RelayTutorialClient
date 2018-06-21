b2e67880220608a9bc6e257f0258e2312b6321f1
KaiCYu <kaichyu@gmail.com>
2018-06-21 16:44:32 -0700

    added react materialize

--- react_components/App.js
+++ react_components/App.js
@@ -1,6 +1,8 @@
 const React = require('react');
 const { graphql, createFragmentContainer } = require('react-relay');
 
+const { Row, Col, CardPanel } = require('react-materialize');
+
 const Welcome = require('./Welcome');
 const Card = require('./Card');
 const UserEdit = require('./UserEdit');
@@ -9,9 +11,29 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Welcome userToGreet={this.props.appUser}/>
-        <Card />
-        <UserEdit activeUser={this.props.appUser}/>
+        <Row>
+          <Col>
+            <CardPanel className="teal lighten-4 black-text">
+              <Welcome userToGreet={this.props.appUser}/>
+            </CardPanel>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={12} m={3}>
+            <CardPanel className="teal lighten-4 black-text">
+              <Card />
+            </CardPanel>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col s={12} m={3}>
+            <CardPanel className="teal lighten-4 black-text">
+              <UserEdit activeUser={this.props.appUser}/>
+            </CardPanel>
+          </Col>
+        </Row>
       </div>
     )
   }
